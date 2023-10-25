1a7f07827ce08aa92accb0b8746c0c7dbbbe8da9
章士祺 <126841600+scchang2023@users.noreply.github.com>
2023-10-25 11:40:16 +0000

    modified

--- exercise/ex_02.py
+++ exercise/ex_02.py
@@ -1,16 +1,14 @@
 # 利用鍵盤輸入兩個整數，透過第三個變數，將兩個整數值交換並輸出。
 # 挑戰：不透過第三個變數，完成兩數交換。
-try:
-    x = int(input("輸入第一個整數 x :"))
-    y = int(input("輸入第二個整數 y :"))
-    # temp = x
-    # x = y
-    # y = temp
-    print(f"x={x}, y={y}")
-    x, y = y, x
-    print(f"兩個數值交換後 x={x}, y={y}")
-except:
-    print("應用程式錯誤")
+
+a = int(input("輸入第一個整數 a :"))
+b = int(input("輸入第二個整數 b :"))
+# temp = a
+# a = b
+# b = temp
+a, b = b, a
+print(f"兩個數值交換後 a={a}, b={b}")
+
 
 # import pyinputplus as pyip
 # x = pyip.inputInt("輸入第一個整數 x :")
