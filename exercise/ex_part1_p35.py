1049590af16378bbd3cc0fb0bfbbf6f18d891ac0
scchang <a0928136004@gmail.com>
2023-10-30 12:34:05 +0800

    modified

--- exercise/ex_part1_p35.py
+++ exercise/ex_part1_p35.py
@@ -1,6 +1,5 @@
 # 利用鍵盤輸入兩個整數，透過第三個變數，將兩個整數值交換並輸出。
 # 挑戰：不透過第三個變數，完成兩數交換。
-
 import pyinputplus as pyip
 a = pyip.inputInt("a=")
 b = pyip.inputInt("b=")
--- exercise/part1_p26.py
+++ exercise/part1_p26.py
@@ -6,5 +6,3 @@ print(f"學生姓名：{name}")
 print(f"數學成績：{Math}")
 print(f"英文成績：{Eng}")
 print(f"總分    ：{Math+Eng}")
-
-
