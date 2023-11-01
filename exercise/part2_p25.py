5e3eef403ed95fe1cf89f77fba5861fb79abfc0f
scchang <a0928136004@gmail.com>
2023-11-01 21:27:06 +0800

    modified

---
+++ exercise/part2_p25.py
@@ -0,0 +1,27 @@
+nums=[]
+n=int(input("input a number:"))
+for i in range(n):
+    num=int(input("input:"))
+    nums.append(num)
+print(nums)
+
+# n=int(input("input a number:"))
+# f=[]
+# f.append(0)
+# f.append(1)
+# n1=1
+# n2=0
+# for i in range(1,10):
+#     n=n1+n2
+#     f.append(n)
+#     n2=n1
+#     n1=n
+# print(f)
+
+# f=[]
+# f.append(0)
+# f.append(1)
+# for i in range(1,10):
+#     f.append(f[-1]+f[-2])
+# print(f)
+
