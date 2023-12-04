04a38d5807647bcc3b9e30d85e0b332813c7989d
scchang <a0928136004@gmail.com>
2023-12-04 14:51:38 +0800

    modified

--- exercise/ex_part4_invoice.py
+++ exercise/ex_part4_invoice.py
@@ -1,5 +1,8 @@
 # 自行選定目標網頁進行爬取。例如：統一發票開獎。
 import requests
+from bs4 import BeautifulSoup
 
 html=requests.get("https://invoice.etax.nat.gov.tw/")
-print(html.text)
+bs=BeautifulSoup(html.text,"html.parser")
+tables=bs.find_all("table")
+print(tables)
